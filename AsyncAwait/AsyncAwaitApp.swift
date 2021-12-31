4de91202396c6c0ea00991afe6a77b83ee3cbb6e
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-31 14:29:26 +0900

    add AsyncAwaitAPP file

---
+++ AsyncAwait/AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitApp.swift
+//  AsyncAwait
+//
+//  Created by 宮本光直 on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
