81cc85b94f533538679dd5f28add2ea769f362d5
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-31 14:30:36 +0900

    add contentVM

---
+++ AsyncAwait/ContentViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ContentViewModel.swift
+//  AsyncAwait
+//
+//  Created by 宮本光直 on 2021/12/31.
+//
+
+import SwiftUI
+
+struct ContentViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentViewModel()
+    }
+}
