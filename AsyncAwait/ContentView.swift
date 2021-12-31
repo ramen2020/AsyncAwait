48951f03b46e9d754d401b68382a9b66a816863a
ramen2020 <vfvsfv.dlk22@gmail.com>
2021-12-31 15:03:39 +0900

    fix contentview

--- AsyncAwait/ContentView.swift
+++ AsyncAwait/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject var viewModel: ContentViewModel
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            ForEach(viewModel.items) { item in
+                GithubItemView(item: item)
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct GithubItemView: View {
+    var item: Github
+
+    var body: some View {
+        HStack {
+            AsyncImage(url: URL(string: item.owner.avatarUrl)) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                ProgressView()
+                    .tint(.cyan)
+            }
+            .frame(width: 40, height: 40)
+            .mask(RoundedRectangle(cornerRadius: 20))
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .frame(width: 50, height: 50)
+            .padding()
+
+            VStack(alignment: .leading, spacing: 2) {
+                Text(item.fullName)
+                    .font(.body)
+                Label(String(item.stargazersCount),
+                      systemImage: "star")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .symbolRenderingMode(.multicolor)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
